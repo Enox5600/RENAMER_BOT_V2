7623abe05478b0991d925df3c8f09bcce8482d2f
Enox5600 <91581852+Enox5600@users.noreply.github.com>
2022-05-25 17:01:46 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -29,8 +29,8 @@ async def start(client,message):
 	**Document Or Video** and enter new filenameto rename it__
 	""",reply_to_message_id = message.message_id ,  
 	reply_markup=InlineKeyboardMarkup(
-	 [[ InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/lntechnical") ], 
-	[InlineKeyboardButton("Subscribe 🧐", url="https://youtube.com/c/LNtechnical") ]  ]))
+	 [[ InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/Oxyver_Support") ], 
+	[InlineKeyboardButton("Join Main Channel 🔰", url="https://t.me/Oxyver") ]  ]))
 
 
 
